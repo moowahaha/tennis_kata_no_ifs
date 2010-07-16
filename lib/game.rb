00cf2e39944d9cb3081ca09b87bfae93a6e2d291
Stephen Hardisty <stephen.hardisty@rea-group.com>
2010-07-16 17:17:03 +1000

    initial commit of ad solution, incomplete

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,90 @@
 class Game
-  def score
-    [0,0]
+  def initialize
+    @player1_score_pos = 0
+    @player2_score_pos = 0
+    @winner = ''
+    @score
+    @scores = [
+            lambda {|player| @score[player - 1] = 0 },
+            lambda {|player| @score[player - 1] = 15},
+            lambda {|player| @score[player - 1] = 30},
+            lambda {|player| @score[player - 1] = 40},
+            lambda {|player|
+              different = @player1_score_pos - @player2_score_pos
+              codes = {
+                -2 => lambda {
+                  @winner = 'Player 2'
+                },
+                0  => lambda {
+                  @score = 'Deuce'
+                },
+                2  => lambda {
+                  @winner = 'Player 1'
+                }
+              }
+
+              codes.
+    }]
+  end
+
+  def player1_score
+    @player1_score_pos += 1
+    @scores[@player1_score_pos].call 1
+  end
+
+  def player2_score
+    @player2_score_pos += 1
+    @scores[@player2_score_pos].call 2
+  end
+
+#  def begining_score
+#      [@scores[@player1_score_pos].call, @scores[@player2_score_pos].call]
+#  end
+#
+#  def score
+#    combo = (@player1_score_pos + @player2_score_pos > 6)
+#    methods = {
+#            true => lambda{begining_score},
+#            false => lambda{end_game_score}
+#            }
+#    method = methods[combo]
+#    method.call
+#  end
+#
+#
+#  def end_game_score
+#    end_scores = {
+#            -1 => 'adv 1',
+#            0 => 'Deuce',
+#            1 => 'adv 2'
+#     }
+#    diff = (@player1_score_pos - @player2_score_pos)
+#    return end_scores[diff]
+#
+#  end
+
+#  def player1_score
+#
+#    increment = lambda { 1 }
+#    score_table = [
+#            #  0            15          30          40
+#            [increment, increment, increment, increment, increment], # 0
+#            [increment, increment, increment, increment, increment], # 0
+#            [increment, increment, increment, increment, increment], # 0
+#            [increment, increment, increment, increment, increment], # 0
+#    ]
+#
+#
+#    @player1_score_pos += 1
+#    @scores[@player1_score_pos].call 1
+#  end
+#
+#  def player2_score
+#    @player2_score_pos += 1
+#    @scores[@player2_score_pos].call 2
+#  end
+
+  def winner
+    @winner
   end
 end
--- spec/tennis_score_spec.rb
+++ spec/tennis_score_spec.rb
@@ -2,8 +2,57 @@ require 'spec'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'game')
 
 describe Game do
+  before do
+    @game = Game.new
+  end
   it "should start at 0-0" do
-    game = Game.new
-    game.score.should == [0,0]
+    @game.score.should == [0,0]
+  end
+
+  it "should be no winner when the game is not started" do
+    @game.winner.should == ''
+  end
+
+  it "should be no winner when the game started" do
+    @game.player1_score
+    @game.winner.should == ''
+  end
+
+  it "should return 15 - 0 if player 1 score" do
+    @game.player1_score
+    @game.score.should == [15, 0]
+  end
+
+  it "should return 15 - 15 if both player score" do
+    @game.player1_score
+    @game.player2_score
+    @game.score.should == [15, 15]
+  end
+
+  it "should return 30 - 15 when player 1 scores twice and player 2 score once" do
+    2.times { @game.player1_score }
+    @game.player2_score
+    @game.score.should == [30, 15]
+  end
+
+  it "should return 40 - 15 when player 1 scores twice and player 2 score once" do
+    3.times { @game.player1_score }
+    @game.score.should == [40, 0]
+  end
+
+  it "should win the game when player 1 scores 4 times uninterrupted" do
+    4.times { @game.player1_score }
+    @game.winner.should == 'Player 1'
+  end
+
+  it "should win the game when player 2 scores 4 times uninterrupted" do
+    4.times { @game.player2_score }
+    @game.winner.should == 'Player 2'
+  end
+
+  it "should be deuce at 40-40" do
+    4.times { @game.player1_score }
+    4.times { @game.player2_score }
+    @game.score.should == 'Deuce'
   end
 end
